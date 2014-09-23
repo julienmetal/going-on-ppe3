e9835c67fc42582466c95d4a24e79be22287b67b
Gregory DAVID <groolot@groolot.net>
2014-09-24 00:12:05 +0200

    englisisation des termes des options

--- programme.py
+++ programme.py
@@ -9,19 +9,19 @@ non_regexp_group = mon_parser_general.add_argument_group("Options simples de fil
 regexp_group = mon_parser_general.add_argument_group("Filtrage RegExp", "Spécifier les termes à rechercher par des expressions rationnelles, auxquels il faut ajouter la quantité souhaitée")
 
 mon_parser_general.add_argument("--time", required=True, type=int, help="Donner la durée totale de la playlist en minutes")
-mon_parser_general.add_argument("--format", nargs=2, help="Donner le format de sortie de la playlist "+str(liste_des_format_de_sortie)+" suivi du nom du fichier de sauvegarde de la playlist (chemin absolu ou relatif, ou '-' pour stdout)")
+mon_parser_general.add_argument("--output", nargs=2, help="Donner le format de sortie de la playlist "+str(liste_des_format_de_sortie)+" suivi du nom du fichier de sauvegarde de la playlist (chemin absolu ou relatif, ou '-' pour stdout)")
 
 non_regexp_group.add_argument("-g", "--genre", nargs=2, help="Spécifier le genre à intégrer à la playlist suivi de la quantité escompté en %%")
 non_regexp_group.add_argument("-s", "--sous-genre", nargs=2, help="Spécifier le sous-genre à intégrer à la playlist suivi de la quantité escompté en %%")
 non_regexp_group.add_argument("-b", "--band", nargs=2, help="Spécifier le nom de l'artiste ou du groupe (band) à intégrer à la playlist suivi de la quantité escompté en %%")
 non_regexp_group.add_argument("-a", "--album", nargs=2, help="Spécifier l'album à intégrer à la playlist suivi de la quantité escompté en %%")
-non_regexp_group.add_argument("-t", "--titre", nargs=2, help="Spécifier le titre à intégrer à la playlist suivi de la quantité escompté en %%")
+non_regexp_group.add_argument("-t", "--title", nargs=2, help="Spécifier le titre à intégrer à la playlist suivi de la quantité escompté en %%")
 
 regexp_group.add_argument("-G", "--RE-genre", nargs=2, help="Spécifier le genre à intégrer à la playlist suivi de la quantité escompté en %%")
-regexp_group.add_argument("-S", "--RE-sous-genre", nargs=2, help="Spécifier le sous-genre à intégrer à la playlist suivi de la quantité escompté en %%")
+regexp_group.add_argument("-S", "--RE-sub-genre", nargs=2, help="Spécifier le sous-genre à intégrer à la playlist suivi de la quantité escompté en %%")
 regexp_group.add_argument("-B", "--RE-band", nargs=2, help="Spécifier le nom de l'artiste ou du groupe (band) à intégrer à la playlist suivi de la quantité escompté en %%")
 regexp_group.add_argument("-A", "--RE-album", nargs=2, help="Spécifier l'album à intégrer à la playlist suivi de la quantité escompté en %%")
-regexp_group.add_argument("-T", "--RE-titre", nargs=2, help="Spécifier le titre à intégrer à la playlist suivi de la quantité escompté en %%")
+regexp_group.add_argument("-T", "--RE-title", nargs=2, help="Spécifier le titre à intégrer à la playlist suivi de la quantité escompté en %%")
 
 
 mes_args = mon_parser_general.parse_args()
