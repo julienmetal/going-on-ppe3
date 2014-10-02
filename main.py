1a23b0e1824adaac1e18e2bab25dc27cd866c39d
Grégory DAVID <groolot@groolot.net>
2014-10-02 17:41:16 +0200

    meilleur présentation

--- main.py
+++ main.py
@@ -36,24 +36,66 @@ def parsingCLI():
     mon_parser_general = argparse.ArgumentParser()
     non_regexp_group = mon_parser_general.add_argument_group("Simple filtering options")
     regexp_group = mon_parser_general.add_argument_group("RegExp filtering")
-    mon_parser_general.add_argument("--time", required=True, type=int, metavar="LENGTH_IN_MINUTES", help="Total playlist length, in minutes")
-    mon_parser_general.add_argument("--log", choices=['DEBUG','INFO','WARNING','ERROR','CRITICAL'], default='WARNING')
-    mon_parser_general.add_argument("--output", nargs=2, metavar=("{"+','.join(format for format in liste_des_formats_de_sortie)+"}", 'FILENAME'), help="Output format {"+','.join(format for format in liste_des_formats_de_sortie)+"} followed by filename (absolute or relative path, or '-' for stdout)", default=['m3u','-'])
+    mon_parser_general.add_argument("--time",
+                                    required=True,
+                                    type=int,
+                                    metavar="LENGTH_IN_MINUTES",
+                                    help="Total playlist length, in minutes")
+    mon_parser_general.add_argument("--log",
+                                    choices=['DEBUG','INFO','WARNING','ERROR','CRITICAL'],
+                                    default='WARNING')
+    mon_parser_general.add_argument("--output",
+                                    nargs=2,
+                                    metavar=("{"+','.join(format for format in liste_des_formats_de_sortie)+"}", 'FILENAME'),
+                                    help="Output format {"+','.join(format for format in liste_des_formats_de_sortie)+"} followed by filename (absolute or relative path, or '-' for stdout)",
+                                    default=['m3u','-'])
 
-    non_regexp_group.add_argument("-g", "--genre", action=appendTypeQuantity, nargs=2, metavar=('GENRE','QUANTITY'), help="Genre to include to playlist, followed by the %% quantity")
-    non_regexp_group.add_argument("-s", "--sub-genre", nargs=2, metavar=('SUB_GENRE','QUANTITY'), help="Sub-genre to include to playlist, followed by the %% quantity")
-    non_regexp_group.add_argument("-b", "--band", nargs=2, metavar=('BAND_NAME','QUANTITY'), help="Band or artist name to include in playlist, followed by the %% quantity")
-    non_regexp_group.add_argument("-a", "--album", nargs=2, metavar=('ALBUM_NAME','QUANTITY'), help="Album title to include in playlist, followed by the %% quantity")
-    non_regexp_group.add_argument("-t", "--title", nargs=2, metavar=('TRACK_TITLE','QUANTITY'), help="Track title to include in playlist, followed by the %% quantity")
+    non_regexp_group.add_argument("-g", "--genre",
+                                  action=appendTypeQuantity,
+                                  nargs=2,
+                                  metavar=('GENRE','QUANTITY'),
+                                  help="Genre to be included to playlist, followed by the %% quantity")
+    non_regexp_group.add_argument("-s", "--sub-genre",
+                                  nargs=2,
+                                  metavar=('SUB_GENRE','QUANTITY'),
+                                  help="Sub-genre to be included to playlist, followed by the %% quantity")
+    non_regexp_group.add_argument("-b", "--band",
+                                  nargs=2,
+                                  metavar=('BAND_NAME','QUANTITY'),
+                                  help="Band or artist name to be included in playlist, followed by the %% quantity")
+    non_regexp_group.add_argument("-a", "--album",
+                                  nargs=2,
+                                  metavar=('ALBUM_NAME','QUANTITY'),
+                                  help="Album title to be included in playlist, followed by the %% quantity")
+    non_regexp_group.add_argument("-t", "--title",
+                                  nargs=2,
+                                  metavar=('TRACK_TITLE','QUANTITY'),
+                                  help="Track title to be included in playlist, followed by the %% quantity")
 
-    regexp_group.add_argument("-G", "--RE-genre", nargs=2, metavar=('GENRE','QUANTITY'), help="Genre to include to playlist, followed by the %% quantity")
-    regexp_group.add_argument("-S", "--RE-sub-genre", nargs=2, metavar=('SUB_GENRE','QUANTITY'), help="Sub-genre to include to playlist, followed by the %% quantity")
-    regexp_group.add_argument("-B", "--RE-band", nargs=2, metavar=('BAND_NAME','QUANTITY'), help="Band or artist name to include in playlist, followed by the %% quantity")
-    regexp_group.add_argument("-A", "--RE-album", nargs=2, metavar=('ALBUM_NAME','QUANTITY'), help="Album title to include in playlist, followed by the %% quantity")
-    regexp_group.add_argument("-T", "--RE-title", nargs=2, metavar=('TRACK_TITLE','QUANTITY'), help="Track title to include in playlist, followed by the %% quantity")
+    regexp_group.add_argument("-G", "--RE-genre",
+                              nargs=2,
+                              metavar=('GENRE','QUANTITY'),
+                              help="Genre to be included to playlist, followed by the %% quantity")
+    regexp_group.add_argument("-S", "--RE-sub-genre",
+                              nargs=2,
+                              metavar=('SUB_GENRE','QUANTITY'),
+                              help="Sub-genre to be included to playlist, followed by the %% quantity")
+    regexp_group.add_argument("-B", "--RE-band",
+                              nargs=2,
+                              metavar=('BAND_NAME','QUANTITY'),
+                              help="Band or artist name to be included in playlist, followed by the %% quantity")
+    regexp_group.add_argument("-A", "--RE-album",
+                              nargs=2,
+                              metavar=('ALBUM_NAME','QUANTITY'),
+                              help="Album title to be included in playlist, followed by the %% quantity")
+    regexp_group.add_argument("-T", "--RE-title",
+                              nargs=2,
+                              metavar=('TRACK_TITLE','QUANTITY'),
+                              help="Track title to be included in playlist, followed by the %% quantity")
     mes_args = mon_parser_general.parse_args()
 
 
+''' Main bloc'''
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s %(filename)s(%(process)s) %(levelname)s:%(message)s')
     
@@ -62,15 +104,11 @@ if __name__ == "__main__":
 
     my_error = 0
     liste_des_formats_de_sortie = ['m3u','xspf','pls']
-    liste_des_options_a_valider_quantite = [
-        'genre','sub_genre','band','album','title',
-        'RE_genre','RE_sub_genre','RE_band','RE_album','RE_title',
-    ]
     mes_args = argparse.Namespace()
-    min_version = (3,2)
+    min_version_python = (3,2)
 
-    if sys.version_info < min_version :
-        logging.critical("Python %i.%i is required" % (min_version[0],min_version[1]))
+    if sys.version_info < min_version_python :
+        logging.critical("Python %i.%i is required" % (min_version_python[0],min_version_python[1]))
         sys.exit(255)
 
     parsingCLI()
