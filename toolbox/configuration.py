be19e8fe043565c3a0cacb67a6a1fb74226313b0
Grégory DAVID <groolot@groolot.net>
2014-10-15 13:15:32 +0200

    database configuration data

--- toolbox/configuration.py
+++ toolbox/configuration.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
 
-liste_des_formats_de_sortie = ['m3u','xspf','pls']
 min_version_python = (3,2)
+liste_des_formats_de_sortie = ['m3u','xspf','pls']
+
+# DATABASE
+DB_ENGINE=''
+DB_USER=''
+DB_USER_PASSWORD=''
+DB_HOST=''
+DB_PORT=''
+DB_DBNAME=''
